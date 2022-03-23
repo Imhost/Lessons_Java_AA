bcd71de7b1221cdb74cc3913c4561635cc3c15ef
Imhost <imhost@ya.ru>
2022-03-23 03:11:57 +0300

    Урок_4, Тест_3

--- src/FirstATest.java
+++ src/FirstATest.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.DeviceRotation;
+import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -34,8 +36,12 @@ public class FirstATest
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
-//    @After
-//    public void tearDown() {driver.quit();}
+    @After
+    public void tearDown()
+    {
+        driver.rotate(ScreenOrientation.PORTRAIT);
+        driver.quit();
+    }
 
     @Test
     public void secondTest()
